d7b63884104f49530d9104b477bdf255fb775943
Your Name <amax@redsymbol.net>
2010-10-10 15:31:51 -0700

    Tool for extracting referrers (read: test data) from Apache "combined" webserver logs

--- tools/extractrefs.py
+++ tools/extractrefs.py
@@ -6,10 +6,26 @@ This tool is used to get test data.
 
 usage:
 
-  extractuas.py < raw-logs.txt > uas.txt
+  extractref.py < raw-logs.txt > refs.txt
   
 You may want to pipe the output through "| sort | uniq".
+
+Records with referrer '-' (meaning unknown referrer) are omitted.
+
+This is a quick-n-dirty, mostly but not totally accurate script.
   
 '''
 
-import pyparsing
+def referrer(logline):
+    '''
+    Extracts the referrer string from an Apache web server "combined" log file record
+
+    '''
+    parts = logline.split('"')
+    return parts[3]
+
+if '__main__' == __name__:
+    import sys
+    for line in sys.stdin:
+        if '-' != line:
+            print referrer(line)
